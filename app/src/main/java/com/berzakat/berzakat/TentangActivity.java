a481245fd9f9e197836c159b97141fa55d1e517f
Joko Priyono <joe.mandrake007@gmail.com>
2018-02-21 14:25:10 +0700

    Penambahan caption rules zakat

--- app/src/main/java/com/berzakat/berzakat/TentangActivity.java
+++ app/src/main/java/com/berzakat/berzakat/TentangActivity.java
@@ -1,7 +1,11 @@
 package com.berzakat.berzakat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TentangActivity extends AppCompatActivity {
 
@@ -9,5 +13,54 @@ public class TentangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tentang);
+
+        ImageView imgTwitter = findViewById(R.id.img_twitter);
+        imgTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                try {
+                    // Jika ada aplikasi twitter yg terinstal
+                    getPackageManager().getPackageInfo("com.twitter.android", 0);
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=ajiiisw"));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                } catch (Exception e) {
+                    // Jika tidak ada langsung ke browser
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/ajiiisw"));
+                }
+                startActivity(intent);
+            }
+        });
+        ImageView imgFacebook = findViewById(R.id.img_facebook);
+        imgFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                try {
+                    getPackageManager().getPackageInfo("com.facebook.katana", 0);
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/ajiiisw"));
+                } catch (Exception e) {
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/ajiiisw"));
+                }
+                startActivity(intent);
+            }
+        });
+        ImageView imgInstagram = findViewById(R.id.img_instagram);
+        imgInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://instagram.com/_u/ajiiisw");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://instagram.com/ajiiisw")));
+                }
+            }
+        });
     }
 }
